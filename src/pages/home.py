9c5654e3cd80cd398d376b201074bbfc9b9b4d2d
JC Castillo <castillochristopherjames@gmail.com>
2024-06-01 00:16:40 +0800

    created food crud

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 
-def render_home(username, render_establishments, render_signin, render_signup, signout):
+def render_home(username, render_establishments, render_foods, render_signin, render_signup, signout):
     # define elements
 
     welcome_message = tk.Label(text="Welcome!")
 
     establishments_button = tk.Button(text="Establishments", command=lambda: render_establishments())
+    foods_button = tk.Button(text="Foods", command=lambda: render_foods())
+
     signin_button = tk.Button(text="Sign In", command=lambda: render_signin())
     signup_button = tk.Button(text="Sign Up", command=lambda: render_signup())
     signout_button = tk.Button(text="Sign Out", command=lambda: signout())
@@ -13,6 +15,7 @@ def render_home(username, render_establishments, render_signin, render_signup, s
     # display elements
     welcome_message.pack()
     establishments_button.pack()
+    foods_button.pack()
 
     if username == None:
         signin_button.pack()
