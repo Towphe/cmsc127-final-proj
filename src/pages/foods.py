9db609188b639ff5d747a1c4e38b056a5257f68d
alyssa2603 <afcea@up.edu.ph>
2024-06-02 04:52:48 +0800

    Report no 2 and 3

--- src/pages/foods.py
+++ src/pages/foods.py
@@ -8,9 +8,65 @@ def render_foods(username:str, foods: pd.DataFrame, home, window:tk.Tk, reposito
         clear_page()
         redirect_to_food_items_view()
         return
+    
+    def load_foods_on_table(foods):
+        row = 1
+        for i in range(foods.shape[0]):
+            food = foods.iloc[i]
+            
+            id_label = tk.Label(table, text=str(food['food_id']), anchor="w")
+            establishment_id_label = tk.Label(table, text=str(food['establishment_id']), anchor="w")
+            name_label = tk.Label(table, text=food['food_name'], anchor="w")
+            category_label = tk.Label(table, text=food['category'], anchor="w")
+            price_label = tk.Label(table, text=food['price'], anchor="w")
+            average_rating_label = tk.Label(table, text=food['average_rating'], anchor="w")
+            edit_button = tk.Button(table, text="Edit", anchor="w", command=lambda fid=food["food_id"]: redirect_to_edit_food_item(fid))
+            delete_button = tk.Button(table, text="Delete", anchor="w", command=lambda fid=food["food_id"]: delete_food(fid))
+            review_button = tk.Button(table, text="Review", anchor="w", command=lambda fid=food["food_id"], eid=food["establishment_id"]: redirect_to_review_food(fid,eid))
+
+            if food["added_by"] != username:
+                # disable edit/delete when username != current login
+                edit_button.config(state=tk.DISABLED, bg='grey')
+                delete_button.config(state=tk.DISABLED, bg='grey')
+            else:
+                review_button.config(state=tk.DISABLED, bg='grey')
+
+            # render rows
+            id_label.grid(row=row, column=0, sticky="ew")
+            establishment_id_label.grid(row=row, column=1, sticky="ew")
+            name_label.grid(row=row, column=2, sticky="ew")
+            category_label.grid(row=row, column=3, sticky="ew")
+            price_label.grid(row=row, column=4, sticky="ew")
+            average_rating_label.grid(row=row, column=5, sticky="ew")
+            edit_button.grid(row=row, column=6, sticky="ew")
+            delete_button.grid(row=row, column=7, sticky="ew")
+            review_button.grid(row=row, column=8, sticky="ew")
+
+            row += 1
+    
+    def empty_table():
+        for row in table.winfo_children():
+            count = 0
+            table.winfo_children()[count].destroy()
+            count = count + 1
+        
+    def searchFood(e):
+        name = search_bar.get()
+    
+        if name == '':
+            data = foods
+        else:
+            data = foods.query(f"food_name.str.contains('{name}', case=False)" )
+            
+        empty_table()
+        load_foods_on_table(data)
         
 
     welcome_message = tk.Label(text="Food View")
+    search_bar = tk.Entry()
+
+    # Search data frame every key release
+    search_bar.bind("<KeyRelease>", searchFood)
 
     back_button = tk.Button(text="Back", command=lambda: home())
 
@@ -27,44 +83,14 @@ def render_foods(username:str, foods: pd.DataFrame, home, window:tk.Tk, reposito
     tk.Label(table, text="Delete", anchor="w").grid(row=0, column=7, sticky="ew")
     tk.Label(table, text="Review", anchor="w").grid(row=0, column=8, sticky="ew")
 
-    row = 1
-    for i in range(foods.shape[0]):
-        food = foods.iloc[i]
-        
-        id_label = tk.Label(table, text=str(food['food_id']), anchor="w")
-        establishment_id_label = tk.Label(table, text=str(food['establishment_id']), anchor="w")
-        name_label = tk.Label(table, text=food['food_name'], anchor="w")
-        category_label = tk.Label(table, text=food['category'], anchor="w")
-        price_label = tk.Label(table, text=food['price'], anchor="w")
-        average_rating_label = tk.Label(table, text=food['average_rating'], anchor="w")
-        edit_button = tk.Button(table, text="Edit", anchor="w", command=lambda fid=food["food_id"]: redirect_to_edit_food_item(fid))
-        delete_button = tk.Button(table, text="Delete", anchor="w", command=lambda fid=food["food_id"]: delete_food(fid))
-        review_button = tk.Button(table, text="Review", anchor="w", command=lambda fid=food["food_id"], eid=food["establishment_id"]: redirect_to_review_food(fid,eid))
-
-        if food["added_by"] != username:
-            # disable edit/delete when username != current login
-            edit_button.config(state=tk.DISABLED, bg='grey')
-            delete_button.config(state=tk.DISABLED, bg='grey')
-        else:
-            review_button.config(state=tk.DISABLED, bg='grey')
-
-        # render rows
-        id_label.grid(row=row, column=0, sticky="ew")
-        establishment_id_label.grid(row=row, column=1, sticky="ew")
-        name_label.grid(row=row, column=2, sticky="ew")
-        category_label.grid(row=row, column=3, sticky="ew")
-        price_label.grid(row=row, column=4, sticky="ew")
-        average_rating_label.grid(row=row, column=5, sticky="ew")
-        edit_button.grid(row=row, column=6, sticky="ew")
-        delete_button.grid(row=row, column=7, sticky="ew")
-        review_button.grid(row=row, column=8, sticky="ew")
-
-        row += 1
+    # Pre-load all foods
+    load_foods_on_table(foods)
     
     add_button = tk.Button(text="Add New Food Item", command=lambda: add_food())
 
     welcome_message.pack()
     back_button.pack()
+    search_bar.pack()
     table.pack()
     add_button.pack()
 
--- src/pages/reports.py
+++ src/pages/reports.py
@@ -1,17 +1,23 @@
 import tkinter as tk
+from tkinter import ttk
+import pandas as pd
 from util.repository import Repository
 
-def render_reports_root(home, view_all_food_establishments, view_establishments_with_high_rating):
+def render_reports_root(home, view_all_food_establishments, view_establishments_with_high_rating, view_all_reviews, view_foods_from_establishment):
     title = tk.Label(text="Reports")
     back_button = tk.Button(text="Back", command=lambda: home())
     view_all_establishments = tk.Button(text="View All Food Establishments", command=lambda: view_all_food_establishments())
     view_high_rated = tk.Button(text="View All Food Establishments with High Ratings", command=lambda: view_establishments_with_high_rating())
+    view_all_reviews_button = tk.Button(text="View All Reviews", command=lambda: view_all_reviews())
+    foods_from_establishment_button = tk.Button(text="Food from establishment", command=lambda: view_foods_from_establishment())
     # create option of reports to view
 
     title.pack()
     back_button.pack()
     view_all_establishments.pack()
     view_high_rated.pack()
+    view_all_reviews_button.pack()
+    foods_from_establishment_button.pack()
 
 def render_view_all_establishments(reports, establishments):
     title = tk.Label(text="View All Establishments")
@@ -74,4 +80,137 @@ def render_establishments_with_high_average_ratings(reports, establishments):
     title.pack()
     back_button.pack()
     table.pack()
-# ----
+
+def render_all_reviews(reports, reviews):
+    def load_data_on_table (reviews):
+        # Load all reviews
+        row = 1
+        for i in range(reviews.shape[0]):
+            review = reviews.iloc[i]
+            name_label = tk.Label(table, text= review['review_for'], anchor="w")
+            username_label = tk.Label(table, text= review['reviewer_username'], anchor="w")
+            rating_label = tk.Label(table, text= review['rating'], anchor="w")
+            content_label = tk.Label(table, text= review['content'], anchor="w")
+            date_label = tk.Label(table, text= review['date_created'], anchor="w")
+
+            name_label.grid(row=row, column=0, sticky="ew")
+            username_label.grid(row=row, column=1, sticky="ew")
+            rating_label.grid(row=row, column=2, sticky="ew")
+            content_label.grid(row=row, column=3, sticky="ew")
+            date_label.grid(row=row, column=4, sticky="ew")
+
+            row += 1
+
+    def empty_table():
+        # empty the table
+        for row in table.winfo_children():
+            count = 0
+            table.winfo_children()[count].destroy()
+
+        
+    def search(e):
+        name = search_bar.get() #get value
+    
+        if name == '':
+            data = reviews
+        else:                   #find review for the given string
+            data = reviews.query(f"review_for.str.contains('{name}', case=False)" )
+        
+        empty_table()
+        load_data_on_table(data)
+
+    title = tk.Label(text="View all reviews")
+    back_button = tk.Button(text="Back", command=lambda: reports())
+    table = tk.LabelFrame()
+
+    search_bar = tk.Entry()
+
+    # Search data frame every key release
+    search_bar.bind("<KeyRelease>", search)
+
+    tk.Label(table, text="Review For", anchor="w").grid(row=0, column=0, sticky="ew")
+    tk.Label(table, text="Reviewee", anchor="w").grid(row=0, column=1, sticky="ew")
+    tk.Label(table, text="Rating", anchor="w").grid(row=0, column=2, sticky="ew")
+    tk.Label(table, text="Content", anchor="w").grid(row=0, column=3, sticky="ew")
+    tk.Label(table, text="Date", anchor="w").grid(row=0, column=4, sticky="ew")
+
+    # Preload table
+    load_data_on_table(reviews)
+    
+    title.pack()
+    back_button.pack()
+    search_bar.pack()
+    table.pack()
+
+def render_foods_from_establishment(reports, establishments, foods, repository):
+    def load_foods_on_table(foods):
+        # Render all food items
+        row = 1
+        for i in range(foods.shape[0]):
+            food = foods.iloc[i]
+            
+            id_label = tk.Label(table, text=str(food['food_id']), anchor="w")
+            establishment_id_label = tk.Label(table, text=str(food['establishment_id']), anchor="w")
+            name_label = tk.Label(table, text=food['food_name'], anchor="w")
+            category_label = tk.Label(table, text=food['category'], anchor="w")
+            price_label = tk.Label(table, text=food['price'], anchor="w")
+            average_rating_label = tk.Label(table, text=food['average_rating'], anchor="w")  
+            # render rows
+            id_label.grid(row=row, column=0, sticky="ew")
+            establishment_id_label.grid(row=row, column=1, sticky="ew")
+            name_label.grid(row=row, column=2, sticky="ew")
+            category_label.grid(row=row, column=3, sticky="ew")
+            price_label.grid(row=row, column=4, sticky="ew")
+            average_rating_label.grid(row=row, column=5, sticky="ew")
+
+            row += 1
+    
+    def empty_table():
+        for row in table.winfo_children():
+            count = 0
+            table.winfo_children()[count].destroy()
+            count = count + 1
+        
+    def searchFood(e):
+        name = search_bar.get()
+        empty_table()
+        
+        if name == '':
+            # If the input is empty, show the full list
+            data = foods
+            load_foods_on_table(data)
+
+        elif (name in establishments['establishment_name'].values):
+            # Get id of selected item
+            id = establishments.loc[establishments['establishment_name'] == name, 'establishment_id'].iloc[0]
+            # Show food items from establishment id
+            data:pd.DataFrame = repository.Reports.all_food_items_from_establishment(id)
+            
+            load_foods_on_table(data)
+
+    welcome_message = tk.Label(text="Food View")
+    search_bar = ttk.Combobox(value=establishments['establishment_name'].tolist())
+
+    # Search data frame every key release and change in choices
+    search_bar.bind("<KeyRelease>", searchFood)
+    search_bar.bind("<<ComboboxSelected>>", searchFood)
+
+    back_button = tk.Button(text="Back", command=lambda: reports())
+
+    # render table
+    table = tk.LabelFrame()
+
+    tk.Label(table, text="Id", anchor="w").grid(row=0, column=0, sticky="ew")
+    tk.Label(table, text="Establishment Id", anchor="w").grid(row=0, column=1, sticky="ew")
+    tk.Label(table, text="Name", anchor="w").grid(row=0, column=2, sticky="ew")
+    tk.Label(table, text="Category", anchor="w").grid(row=0, column=3, sticky="ew")
+    tk.Label(table, text="Price", anchor="w").grid(row=0, column=4, sticky="ew")
+    tk.Label(table, text="Average Rating", anchor="w").grid(row=0, column=5, sticky="ew")
+
+    # Pre-load all foods
+    load_foods_on_table(foods)
+
+    welcome_message.pack()
+    back_button.pack()
+    search_bar.pack()
+    table.pack()
